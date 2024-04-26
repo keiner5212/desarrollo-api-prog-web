e62e067e20b79cf765ad8b697f9bd83a8c9c2197
Keiner Alvarado <kjgameplay0000@gmail.com>
2024-04-26 17:32:20 -0500

    fix: sesion duration fix

--- src/main/java/eshop/prod/utils/constants/Security.java
+++ src/main/java/eshop/prod/utils/constants/Security.java
@@ -3,5 +3,6 @@ package eshop.prod.utils.constants;
 public class Security {
     public static final String UserRole = "USER";
     public static final String AdminRole = "ADMIN";
-    public static final long JWT_EXPIRATION = 70000;
+    public static final long JWT_EXPIRATION = 3600000; // 1 hora en milisegundos (duracion de la validez del token/sesión)
+
 }
