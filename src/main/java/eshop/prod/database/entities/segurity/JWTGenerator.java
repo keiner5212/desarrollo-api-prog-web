e93cd1392541e438e5be66582cae4ece2222ce34
Keiner Alvarado <kjgameplay0000@gmail.com>
2024-04-30 20:41:45 -0500

    fix: jwt secret

--- src/main/java/eshop/prod/database/entities/segurity/JWTGenerator.java
+++ src/main/java/eshop/prod/database/entities/segurity/JWTGenerator.java
@@ -1,14 +1,17 @@
 package eshop.prod.database.entities.segurity;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 import javax.crypto.SecretKey;
 
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
 
 import io.jsonwebtoken.Claims;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
@@ -18,43 +21,50 @@ import eshop.prod.utils.constants.Security;
 @Component
 @Slf4j
 public class JWTGenerator {
-	private static final SecretKey key = Jwts.SIG.HS512.key().build();
+	@Value("${jwt.secretKey}")
+	private static String secretKeyString;
 	
+	// this generates a new key every time the server restarts (may be a problem)
+	// private static final SecretKey key = Jwts.SIG.HS512.key().build();
+
+	private static final SecretKey key = Keys.hmacShaKeyFor(secretKeyString.getBytes(StandardCharsets.UTF_8));
+
 	public String generateToken(Authentication authentication) {
-        
+
 		String username = authentication.getName();
 		Date currentDate = new Date();
 		Date expireDate = new Date(currentDate.getTime() + Security.JWT_EXPIRATION);
-		
+
 		String token = Jwts.builder()
 				.subject(username)
 				.issuedAt(new Date())
 				.expiration(expireDate)
 				.signWith(key)
 				.compact();
-                
-        log.info("Token generated: " + token);
+
+		log.info("Token generated: " + token);
 		return token;
 	}
 
-	public String getUsernameFromJWT(String token){
+	public String getUsernameFromJWT(String token) {
 		Claims claims = Jwts.parser()
-                .verifyWith(key)
+				.verifyWith(key)
 				.build()
 				.parseSignedClaims(token)
 				.getPayload();
 		return claims.getSubject();
 	}
-	
+
 	public boolean validateToken(String token) {
 		try {
 			Jwts.parser()
-            .verifyWith(key)
-			.build()
-			.parseSignedClaims(token);
+					.verifyWith(key)
+					.build()
+					.parseSignedClaims(token);
 			return true;
 		} catch (Exception ex) {
-			throw new AuthenticationCredentialsNotFoundException("JWT was exprired or incorrect",ex.fillInStackTrace());
+			throw new AuthenticationCredentialsNotFoundException("JWT was exprired or incorrect",
+					ex.fillInStackTrace());
 		}
 	}
 
